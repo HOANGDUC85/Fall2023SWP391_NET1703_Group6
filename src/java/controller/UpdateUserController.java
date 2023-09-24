b36c034a5043cbd3dea478fbd04421695b80fac6
Hoang Dinh Duc <144517413+HOANGDUC85@users.noreply.github.com>
2023-09-24 20:31:45 +0700

    Update UpdateUserController.java

--- src/java/controller/UpdateUserController.java
+++ src/java/controller/UpdateUserController.java
@@ -34,6 +34,8 @@ public class UpdateUserController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
         String url = ERROR;
         try {
             String lastSearchValue = request.getParameter("search");
